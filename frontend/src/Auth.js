e0e068db6b92bbb0135b7f73792b8b1554ee559e
Keren Temkin <kerentem@gmail.com>
2023-03-02 22:52:47 +0200

    add sign in/sign out UI

---
+++ frontend/src/Auth.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react"
+
+export default function (props) {
+    let [authMode, setAuthMode] = useState("signin")
+    let [name, setName] = useState();
+    let [mail, setMail] = useState();
+    let [password, setPassword] = useState();
+
+    const changeAuthMode = () => {
+        setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+
+    if (authMode === "signin") {
+        return (
+            <div className="Auth-form-container">
+                <form className="Auth-form">
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Sign In</h3>
+                        <div className="text-center">
+                            Not registered yet?{" "}
+                            <span className="link-primary" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Email address</label>
+                            <input
+                                type="email"
+                                className="form-control mt-1"
+                                placeholder="Enter email"
+                                onChange={e => setMail(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control mt-1"
+                                placeholder="Enter password"
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="d-grid gap-2 mt-3">
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
+                        </div>
+                        <p className="text-center mt-2">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    else return (
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign Up</h3>
+                    <div className="text-center">
+                        Already registered?{" "}
+                        <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Full Name</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="e.g Israel Israeli"
+                            onChange={e => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Email Address"
+                            onChange={e => setMail(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Password"
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="text-center mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    )
+}
