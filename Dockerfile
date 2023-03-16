6bb5f0da4680c068bfce390c5b36192420cfa085
bar.sela <bar.sela@ridewithvia.com>
2023-03-16 09:02:48 +0200

    mysql and docker

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.9-slim-buster
+
+# Set the working directory in the container
+WORKDIR /wifix
+
+# Copy the requirements.txt file and install the dependencies
+# Installing client libraries and any other package you need
+
+COPY backend/requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code to the container
+COPY .. .
+
+# Expose the port that the Flask application will run on
+EXPOSE 8080
+
+# Start the Flask application
+CMD ["python", "backend/db_server/app.py"]
--- backend/db_server/app.py
+++ backend/db_server/app.py
@@ -2,23 +2,22 @@ import os
 from typing import Optional
 from datetime import datetime
 from flask import Flask, request
-import psycopg2
 import logging
 
-from backend.db_server.exception import UniqueViolationException, InvalidUsernameException
-from backend.db_server.validation import validate_register_request, validate_credit_card
+from exception import UniqueViolationException, InvalidUsernameException
+from validation import validate_register_request, validate_credit_card
 from werkzeug.security import generate_password_hash
-from postgresql_query import *
+from MySQL_query import *
 from utiles import make_db_server_response, HttpStatus
+import mysql.connector
 
 db_server = Flask(__name__)
+
 RDS_ENDPOINT = os.environ["RDS_ENDPOINT"]
 RDS_USERNAME = os.environ["RDS_USERNAME"]
 RDS_PASSWORD = os.environ["RDS_PASSWORD"]
-RDS_PORT = 5432
-DATABASE = "wifix-db"
-connection = None
-cursor = None
+RDS_PORT = 3306
+DATABASE = "wifix_db"
 
 logger = logging.getLogger(__name__)
 
@@ -28,18 +27,18 @@ def register():
     # Get the user's registration information from the request
     data = request.get_json()
 
-    username: str = data['username']
+    full_name: str = data['full_name']
     password: str = data['password']
     email: str = data['email']
 
-    validate_register_request(username, password, email)
+    validate_register_request(full_name, password, email)
 
     hashed_password: str = generate_password_hash(password)
 
     try:
         cursor.execute(
             INSERT_A_USER_QUERY,
-            (username, hashed_password, email)
+            (full_name, email, hashed_password)
         )
 
         connection.commit()
@@ -51,21 +50,21 @@ def register():
 
         return response
 
-    except psycopg2.errors.UniqueViolation as error:
+    except mysql.connector.errors.IntegrityError as error:
         connection.commit()
-        raise UniqueViolationException(error_msg=error.error.pgerror)
+        raise UniqueViolationException(error_msg=error.msg)
 
-    except psycopg2.Error as error:
+    except mysql.connector.errors as error:
         connection.commit()
-        raise Exception(f"Error while creating a new user with PostgreSQL,\n"
-                        f"Error: {error}")
+        raise Exception(f"Error while creating a new user with MySQL,\n"
+                        f"Error: {error.msg}")
 
 
 @db_server.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
 
-    username: str = data['username']
+    email: str = data['email']
     password: str = data['password']
 
     hashed_password: str = generate_password_hash(password)
@@ -73,7 +72,7 @@ def login():
     try:
         cursor.execute(
             IS_USER_EXISTS_QUERY,
-            (username, hashed_password)
+            (email, hashed_password)
         )
 
         user = cursor.fetchone()
@@ -85,8 +84,8 @@ def login():
         else:
             raise Exception
 
-    except psycopg2.Error as error:
-        raise Exception(f"Error while creating a new user with PostgreSQL,\n"
+    except mysql.connector.errors as error:
+        raise Exception(f"Error while creating a new user with MySQL,\n"
                         f"Error: {error}")
 
 
@@ -99,7 +98,7 @@ def add_card():
     expiration_month: str = data['exp_month']
     expiration_year: str = data['exp_year']
     cvv: str = data['cvv']
-    username: str = data['username']
+    email: str = data['email']
 
     validate_credit_card(card_number)
 
@@ -109,7 +108,7 @@ def add_card():
     try:
         cursor.execute(
             INSERT_CREDIT_CARD_BY_USER_ID_QUERY,
-            (username, card_number, expiration_month, expiration_year, hashed_cvv))
+            (email, card_number, expiration_month, expiration_year, hashed_cvv))
 
         connection.commit()
 
@@ -118,15 +117,15 @@ def add_card():
 
         return response
 
-    except psycopg2.errors.ForeignKeyViolation as error:
+    except mysql.connector.errors.IntegrityError as error:
         connection.commit()
-        logger.error(f"Error while inserting credit card with PostgreSQL, error: {error}")
-        raise InvalidUsernameException(f"Hi {username},"
+        logger.error(f"Error while inserting credit card with MySQL, error: {error}")
+        raise InvalidUsernameException(f"Hi {email},"
                                        "\nPlease register first.")
 
-    except psycopg2.Error as error:
+    except mysql.connector.errors as error:
         connection.commit()
-        logger.error(f"Error while inserting credit card with PostgreSQL,\n"
+        logger.error(f"Error while inserting credit card with MySQL,\n"
                      f"Error: {error}")
         raise error
 
@@ -135,7 +134,7 @@ def add_card():
 def start_wifi_session():
     data = request.get_json()
 
-    username: str = data['username']
+    email: str = data['email']
     data_usage: Optional[int] = data.get('data_usage') if data.get('data_usage') else 0
 
     if data.get('start_time'):
@@ -152,35 +151,35 @@ def start_wifi_session():
     try:
         cursor.execute(
             INSERT_WIFI_SESSION_QUERY,
-            (username, start_time, end_time, data_usage))
+            (email, start_time, end_time, data_usage))
 
         connection.commit()
 
-        msg = f'Added WiFi session to user: {username} successfully'
+        msg = f'Added WiFi session to user: {email} successfully'
         response = make_db_server_response(HttpStatus.OK, msg, {})
 
         return response
 
-    except psycopg2.errors.ForeignKeyViolation as error:
+    except mysql.connector.IntegrityError as error:
         connection.commit()
-        logger.error(f"Error while inserting credit card with PostgreSQL, error: {error}")
-        raise InvalidUsernameException(f"Hi {username},"
+        logger.error(f"Error while inserting credit card with MySQL, error: {error}")
+        raise InvalidUsernameException(f"Hi {email},"
                                        "\nPlease register first.")
 
-    except psycopg2.Error as error:
+    except mysql.connector.errors as error:
         connection.commit()
-        logger.error(f"Error while inserting WiFi session with PostgreSQL,\n"
+        logger.error(f"Error while inserting WiFi session with MySQL,\n"
                      f"Error: {error}")
         raise error
 
 
 @db_server.route('/get_wifi_session/end_time', methods=['Get'])
 def get_wifi_session_end_time():
-    username = request.args['username']
+    email = request.args['email']
 
     try:
         cursor.execute(
-            GET_WIFI_SESSION_END_TIME_QUERY, (username,)
+            GET_WIFI_SESSION_END_TIME_QUERY, (email,)
         )
 
         results = cursor.fetchall()[0]
@@ -195,45 +194,49 @@ def get_wifi_session_end_time():
 
         return response
 
-    except psycopg2.errors.ForeignKeyViolation as error:
-        logger.error(f"Error while inserting credit card with PostgreSQL, error: {error}")
-        raise InvalidUsernameException(f"Hi {username},"
+    except mysql.connector.errors.IntegrityError as error:
+        logger.error(f"Error while inserting credit card with MySQL, error: {error}")
+        raise InvalidUsernameException(f"Hi {email},"
                                        "\nPlease register first.")
 
-    except psycopg2.Error as error:
-        logger.error(f"Error while inserting WiFi session with PostgreSQL,\n"
+    except mysql.connector.errors as error:
+        logger.error(f"Error while inserting WiFi session with MySQL,\n"
                      f"Error: {error}")
         raise error
 
 
 def creates_tables(connection, cursor):
-    cursor.execute(POSTGRESQL_CREATE_USERS_TABLE_QUERY)
-    cursor.execute(POSTGRESQL_CREATE_CREDIT_CARD_TABLE_QUERY)
-    cursor.execute(POSTGRESQL_CREATE_WIFI_SESSION_TABLE_QUERY)
-    connection.commit()
+    if connection and cursor:
+        cursor.execute(MySQL_CREATE_USERS_TABLE_QUERY)
+        cursor.execute(MySQL_CREATE_CREDIT_CARD_TABLE_QUERY)
+        cursor.execute(MySQL_CREATE_WIFI_SESSION_TABLE_QUERY)
+        connection.commit()
 
 
+connection = None
+cursor = None
 try:
-    logger.info('PostgreSQL is connecting')
-    connection = psycopg2.connect(user=RDS_USERNAME,
-                                  password=RDS_PASSWORD,
-                                  host=RDS_ENDPOINT,
-                                  port=RDS_PORT,
-                                  database=DATABASE)
+    logger.info('MySQL is connecting')
+    connection = mysql.connector.connect(user=RDS_USERNAME, password=RDS_PASSWORD,
+                                         host=RDS_ENDPOINT, database=DATABASE, port=RDS_PORT)
     cursor = connection.cursor()
-    creates_tables(connection, cursor)
-    db_server.run(debug=True, port=8080)
 
-except(Exception, psycopg2.Error) as error:
-    logger.error("Error from PostgreSQL", error)
 except Exception as error:
     logger.error(f"Generic error: {error}")
 
 
-finally:
-    # closing database connection.
-    if connection:
-        logger.info('Closing PostgreSQL database connection.')
-        cursor.close()
-        connection.close()
-        logger.info("PostgreSQL connection is closed")
+def main():
+    try:
+        creates_tables(connection, cursor)
+        db_server.run(debug=True, port=8080, host='0.0.0.0')
+    finally:
+        # closing database connection.
+        if connection:
+            logger.info('Closing MySQL database connection.')
+            cursor.close()
+            connection.close()
+            logger.info("MySQL connection is closed")
+
+
+if __name__ == "__main__":
+    main()
