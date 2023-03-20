9e2d6735adece38a9a5aba9df2f9c2eba05bc504
michaelkosoy <michaelks@mta.ac.il>
2023-03-20 17:17:52 +0200

    alpha version

--- backend/router_live_data/router_data_feed.py
+++ backend/router_live_data/router_data_feed.py
@@ -1,12 +1,12 @@
 import psutil
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "admin"
+
 
 def init_and_login():
     # Navigate to the router's web interface
@@ -26,33 +26,73 @@ def init_and_login():
     driver.implicitly_wait(10)
 
 
-# Find the router's information
-text: str = "//*[@id=/'a38/']"
-driver.switch_to.frame("bottomLeftFrame")
-driver.find_element(By.XPATH, '//*[@id="ol38"]').click()
-driver.switch_to.default_content()
-driver.switch_to.frame("mainFrame")
+def limit_upload_download_speed(upload_speed, download_speed):
+    # Find the router's information
+    driver.switch_to.frame("bottomLeftFrame")
+    driver.find_element(By.XPATH, '//*[@id="ol38"]').click()
+    driver.switch_to.default_content()
+    driver.switch_to.frame("mainFrame")
+    egress_bandwidth_field = driver.find_element(By.XPATH,
+                                                 '/html/body/form/center/table/tbody/tr[3]/td/table/tbody/tr[3]/td['
+                                                 '2]/input')
+    egress_bandwidth_field.clear()
+    egress_bandwidth_field.send_keys(upload_speed)
+    egress_bandwidth_field = driver.find_element(By.XPATH,
+                                                 '/html/body/form/center/table/tbody/tr[3]/td/table/tbody/tr[4]/td['
+                                                 '2]/input')
+    egress_bandwidth_field.clear()
+    egress_bandwidth_field.send_keys(download_speed)
+    router_info = egress_bandwidth_field
+    print(router_info)
 
-egress_bandwidth_field = driver.find_element(By.XPATH,
-                                             '//*[@id="t_up_band"]')
-router_info = egress_bandwidth_field
-print(router_info)
 
-# Close the webdriver
-driver.quit()
+def print_general_packets_data():
+    # Get the network interface for internet usage
+    interface = psutil.net_io_counters(pernic=True)['WiFi']
 
-# Get the network interface for internet usage
-interface = psutil.net_io_counters(pernic=True)['WiFi']
+    # Get the total number of bytes sent and received
+    bytes_sent: int = interface.packets_sent
+    bytes_recv: int = interface.packets_recv
 
-# Get the total number of bytes sent and received
-bytes_sent: int = interface.packets_sent
-bytes_recv: int = interface.packets_recv
+    # Calculate the total amount of data used
+    total_data_used: int = bytes_sent + bytes_recv
 
-# Calculate the total amount of data used
-total_data_used: int = bytes_sent + bytes_recv
+    # Print the result
+    print("Total internet data used: ", total_data_used, " packets")
 
-# Print the result
-print("Total internet data used: ", total_data_used, " packets")
+
+def get_wireless_customer_data() -> dict:
+    driver.switch_to.default_content()
+    driver.switch_to.frame("bottomLeftFrame")
+    driver.find_element(By.XPATH, '//*[@id="a7"]').click()
+    driver.find_element(By.XPATH, '//*[@id="a12"]').click()
+    driver.switch_to.default_content()
+    driver.switch_to.frame("mainFrame")
+    amount_connected_devices = driver.find_element(By.XPATH, '//*[@id="autoWidth"]/tbody/tr[3]/td[2]').text
+    amount_range = range(int(amount_connected_devices))
+    result_usage_dict: dict = {}
+    for connection in amount_range:
+        connection_index: int = 2 + connection
+        connection_recieved_packets = driver.find_element(By.XPATH, '//*[@id="autoWidth"]/tbody/tr['
+                                                                    '5]/td/table/tbody/tr[{}]/td[4]'.
+                                                          format(connection_index)).text
+
+        connection_sent_packets = driver.find_element(By.XPATH,
+                                                      '//*[@id="autoWidth"]/tbody/tr[5]/td/table/tbody/tr[{}]/td[5]'.
+                                                      format(connection_index)).text
+        result_usage_dict["{}".format(connection)] = [connection_recieved_packets, connection_sent_packets]
+    return result_usage_dict
+
+
+def create_output_json(data_usage):
+    with open("users_data.json", "w") as outfile:
+        json.dump(data_usage, outfile)
+
 
 if __name__ == '__main__':
-    print('PyCharm')
+    init_and_login()
+    limit_upload_download_speed(download_speed='2049', upload_speed='513')
+    print_general_packets_data()
+    wireless_data: dict = get_wireless_customer_data()
+    create_output_json(data_usage=wireless_data)
+    driver.quit()
